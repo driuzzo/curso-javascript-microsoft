ca3a6add5bc48dc3b9972b146926cd16b99def01
Adriano Driuzzo <adriano.driuzzo@hotmail.com>
2021-07-25 15:21:18 -0300

    more files

---
+++ functions.js
@@ -0,0 +1,7 @@
+function printHello(name) {
+    console.log("Hello "+name);
+    return "name"+" hello!";
+}
+let result = printHello("Adriano");
+console.log(result)
+
---
+++ loops.js
@@ -0,0 +1,21 @@
+const names = ['Justin', 'Burke', 'Sarah'];
+
+console.log('-- while loop --');
+let whileIndex = 0;
+while (whileIndex < names.length) {
+    const name = names[whileIndex];
+    console.log(name);
+    whileIndex++;
+}
+
+console.log('-- for loop --');
+for (let forIndex = 0; forIndex < names.length; forIndex++) {
+    const name = names[forIndex];
+    console.log(name)
+}
+
+console.log('-- for of loop --');
+
+for (const name of names) {
+    console.log(name);
+}
